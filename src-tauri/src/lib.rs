2745cf64fe79769e4ea4f52c81cce79e5496dbc5
hong <daggax8235@gmail.com>
2025-12-08 22:43:40 +0900

    chore: Remove crop_voice_to_limit from lib.rs exports

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,5 +4,5 @@ pub mod converter;
 
 pub use converter::{
     extract_midi_notes, allocate_voices_smart, generate_mml_final,
-    crop_voice_to_limit, Note, TPB, GRID_SIZE,
+    Note, TPB, GRID_SIZE,
 };
